f5400f88c48c01527a659e7f57260db55d648b6c
Suhong Jin <suhongjin94@gmail.com>
2015-12-17 23:40:40 -0600

    update history page to show moves

--- static/js/getList.js
+++ static/js/getList.js
@@ -1,4 +1,8 @@
- $.post("/history", function(data) {
+var currentMoveList = null;
+var currPosition = 0;
+var movesByGame = new Array();
+
+$.post("/history", function(data) {
  	var gameList = document.getElementById("game-list");
  	for (var i = 0; i < data.length; i++) {
  		var newGameItem = document.createElement("li");
@@ -6,11 +10,14 @@
         //create new text node
         var gameNumberItem = document.createTextNode("Game: " + data[i].game);
 
-
+        movesByGame.push(data[i].moves);
         //add text node to li element
         newGameItem.appendChild(gameNumberItem);
         if (i == 0) {
+        	var gameTitle = document.getElementById("game-title");
         	newGameItem.className = "selected";
+        	gameTitle.innerText = "Game: " + data[i].game;
+        	currentMoveList = data[i].moves;
         }
 
 
@@ -22,6 +29,41 @@
  	$('.sidebar-nav li').click(function(e) {
         $('.sidebar-nav li').removeClass('selected');
         $(this).addClass('selected');
+        currentMoveList = movesByGame[($(this).index())];
+        currPosition = 0;
+        clearBoard();
         e.preventDefault();
     });
 });
+
+function previousMove() {
+	if (currPosition > 0) {
+		currPosition--;
+		var squarePosition = currentMoveList[currPosition]
+		var square = document.getElementById(squarePosition[0] * 3 + squarePosition[1])
+		square.innerHTML = "";
+	}
+
+
+}
+
+function nextMove() {
+	if (currPosition < currentMoveList.length) {
+		var squarePosition = currentMoveList[currPosition]
+		var square = document.getElementById(squarePosition[0] * 3 + squarePosition[1]);
+		if (currPosition % 2 == 0) {
+			square.innerHTML = "X";
+		}
+		else {
+			square.innerHTML = "O";
+		}
+		currPosition++;
+	}
+}
+
+function clearBoard() {
+	var squares = document.getElementsByClassName("square");
+	for (var s = 0; s < squares.length; s++) {
+		squares[s].innerHTML = "";
+	}
+}
